c2a9e142b4e3941562315c058a9e0831dcc0093f
Gleisson Braga <135471754+gleissonbraga@users.noreply.github.com>
2026-06-30 20:39:53 -0300

    ajustando o bug referente a não existir a divisão por 100

--- calcular.py
+++ calcular.py
@@ -5,4 +5,4 @@ def calcular_preco_final(valor, desconto):
     desconto em porcentagem
     Ex.: 10 = 10%
     """
-    return valor - (valor * desconto)
+    return valor - (valor * desconto / 100)
